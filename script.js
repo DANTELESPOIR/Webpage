71f66798260acb9382e4d1119d16ebc4ed1d318e
DANTELESPOIR <danylespoir@gmail.com>
2021-05-15 12:35:42 +0400

    js alert fix

--- script.js
+++ script.js
@@ -1,2 +1,2 @@
 alert( 'Welcome to my web page' );
-alert('Please if you would like me to add something to make it better please e-mail me at danylespoir@gmail.com');
+alert('Please if you would like me to add something to make it better \nplease e-mail me at danylespoir@gmail.com');
