78802ba309c2e4365b2e7293565f57b2988340de
Dante Lespoir <danylespoir@gmail.com>
2021-07-18 15:36:46 +0400

    Not much
    just messing around

--- script.js
+++ script.js
@@ -1,2 +1,1 @@
-alert( 'Welcome to my web page' );
-alert('Please if you would like me to add something to make it better \nplease e-mail me at danylespoir@gmail.com');
+alert( 'Welcome to my websie');
