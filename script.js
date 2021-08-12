740cbd85b1d9d828831c24bb1c399479ecc43d0e
Dante Lespoir <danylespoir@gmail.com>
2021-08-12 21:48:51 +0400

    trying out javascript

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
-var x = 5 
+/*
+Just trying out javascript
+ */
+
+var x = 5
 var y = 10 
 
 console.log( x + y);
 console.log( y - x);
 console.log( x * y);
 console.log( x / y);
+
+{
+alert("Are you sure you want to go here :p");
+}
+
 
