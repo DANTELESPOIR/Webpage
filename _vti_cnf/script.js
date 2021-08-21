5935d3af63ed5d5bc71c8e9b33deabb56e6545a4
Dante Lespoir <danylespoir@gmail.com>
2021-08-21 18:56:39 +0400

    ok

--- _vti_cnf/script.js
+++
@@ -1,7 +0,0 @@
-vti_encoding:SR|utf8-nl
-vti_timelastmodified:TR|18 Jul 2021 17:10:46 -0000
-vti_extenderversion:SR|6.0.2.5516
-vti_cacheddtm:TX|05 Jul 2021 14:54:06 -0000
-vti_filesize:IR|31
-vti_backlinkinfo:VX|Webpage/index.html
-vti_modifiedby:SR|DESKTOP-C98F1BE\\Dante Lespoir
--- script.js
+++ script.js
@@ -12,6 +12,9 @@ console.log( x / y);
 
 //alert("Are you sure you want to go here :D");
 
+/*This promps a message asking for a users name but it will keep showing up everytime the page reloads*/
+//var text = prompt("What is your name, ");
+//alert("Hello ", text);
 
 
 
