8a0e2f52921fc302f2d7d818b133d13589792cbc
Dante Lespoir <danylespoir@gmail.com>
2021-10-18 17:31:21 +0400

    Alinement changes and image

--- script.js
+++ script.js
@@ -2,15 +2,34 @@
 Just trying out javascript
  */
  
-var x = 5
+/*var x = 5
 var y = 10 
 
 console.log( x + y);
 console.log( y - x);
 console.log( x * y);
 console.log( x / y);
+*/
 
 
+/*
+player = prompt("Type User player name please");
+
+Mian_player = "Dante";
+
+var Mian_player_HP= 100
+var Player_HP= 100
+
+console.log("Player has joined the game");
+console.log("Player , " + player , "Has joined the game");
+console.log("Player" + player , "Another player by the username " + Mian_player + " Has joined");
+console.log("He request to have a dule");
+console.log("Do you accept");
+console.log("Do you wish to fight");
+
+console.log("You have a simple Game of Chess");
+console.log("");
+*/
 
 //alert("Are you sure you want to go here :D");
 
